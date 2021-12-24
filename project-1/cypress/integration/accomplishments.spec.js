81620041bf0dff8e3ec4320c9606d709cee53c69
Umut Akin <umutakin@fastmail.com>
2021-12-24 14:46:56 +0300

    cypress crash course (laith harb) - tests - accomplishments

---
+++ project-1/cypress/integration/accomplishments.spec.js
@@ -0,0 +1,48 @@
+/// <reference types='cypress' />
+
+describe("Accomplishments Dashboard", () => {
+  beforeEach(() => {
+    cy.visit("/accomplishments");
+  });
+
+  it("should showcase error if information is missing", () => {
+    cy.get("[data-cy='accomplishment-title-input']").type(
+      "My Basketball Accomplishment"
+    );
+    cy.get("[data-cy='accomplishment-input']").type(
+      "I made 10 threes in a row"
+    );
+    cy.contains("Submit Accomplishment").click();
+    cy.contains("Complete the items above to continue").should("be.visible");
+  });
+
+  it("should display validation component if all information is input", () => {
+    cy.get("[data-cy='accomplishment-title-input']").type(
+      "My Basketball Accomplishment"
+    );
+    cy.get("[data-cy='accomplishment-input']").type(
+      "I made 10 threes in a row"
+    );
+    cy.get("[type='checkbox']").click();
+    cy.contains("Submit Accomplishment").click();
+    cy.contains("This Accomplishment was Successfully Submitted").should(
+      "be.visible"
+    );
+  });
+
+  it("should return back to accomplishment dashboard with empty inputs when go back button is clicked", () => {
+    cy.get("[data-cy='accomplishment-title-input']").type(
+      "My Basketball Accomplishment"
+    );
+    cy.get("[data-cy='accomplishment-input']").type(
+      "I made 10 threes in a row"
+    );
+    cy.get("[type='checkbox']").click();
+    cy.contains("Submit Accomplishment").click();
+    cy.contains("Go Back").click();
+    cy.contains("h2", "Accomplishment").should("be.visible");
+    cy.get("[data-cy='accomplishment-title-input']").should("have.value", "");
+    cy.get("[data-cy='accomplishment-input']").should("have.value", "");
+    cy.get("[type='checkbox']").should("not.be.checked");
+  });
+});
